d1190f5f51d36d3e01db2c67969c7c328feaae83
Advaita <135300205+A-dvaita@users.noreply.github.com>
2025-05-04 20:42:00 +0530

    Use some variables and a while loop to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = (string) => {
+    let newString = "";
+    let myString = string;
+    let length = myString.length;
+    let lastChar = myString.charAt(length - 1);
+    while(length>=0){
+        newString += lastChar;
+        length = length - 1;
+        lastChar = myString.charAt(length-1);
+    }
+    return newString;
 };
 
 // Do not edit below this line
