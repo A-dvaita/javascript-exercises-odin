da8ee4c201b37d9a939eb3387349497fb46af760
Advaita <135300205+A-dvaita@users.noreply.github.com>
2025-05-05 12:39:10 +0530

    Use arithmetic swapping in case num1 > num2

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
+    if(num1 > num2){
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+    }
     for (let i = num1; i <= num2; i++){
         sum += num1;
         ++num1;
