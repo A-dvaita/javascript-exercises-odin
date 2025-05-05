192bfce16187ba4c975d42b1d8af2feb7aa6556d
Advaita <135300205+A-dvaita@users.noreply.github.com>
2025-05-05 12:50:12 +0530

    Use condition to check if num1 and num2 are integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -5,6 +5,12 @@ const sumAll = function(num1, num2) {
         num2 = num1 - num2;
         num1 = num1 - num2;
     }
+    if(num1 < 0 || num2 < 0 || num1 && num2 < 0){
+        return 'ERROR';
+    }
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    }
     for (let i = num1; i <= num2; i++){
         sum += num1;
         ++num1;
