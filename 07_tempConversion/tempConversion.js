0bcaba921e5d9d23282b9a469c54aeeec7eb5307
Advaita <135300205+A-dvaita@users.noreply.github.com>
2025-05-05 15:43:40 +0530

    Round off converted temperature to one decimal place
    This is done using Math.round method.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempToC = ((5/9) * (tempF - 32));
+  return (Math.round(tempToC * 10)/10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempToF = (((9/5) * tempC) + 32);
+  return (Math.round(tempToF * 10)/10);
 };
 
 // Do not edit below this line
